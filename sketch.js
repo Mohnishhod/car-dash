67c664218d0fb81d93d8ccc736e87f2ca1794402
Mohnishhod <68765506+Mohnishhod@users.noreply.github.com>
2020-08-16 09:03:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function draw() {
   background(0);
   if( wall.x - car.x < (car.width + wall.width)/2)
        {
-      car.velocity.X = 0;
+      car.velocityX = 0;
       var deformation =0.5 * weight * speed* speed/22509;
         if(deformation>180)
         {
